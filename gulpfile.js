6443adca0e6c6b58af3f5e55b7147f94ea20170f
Juliana Basilio <jully.morgan@hotmail.com>
2019-03-26 14:08:30 -0300

    projeto final

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,32 @@
+// Variables
 var gulp    = require('gulp');
 var sass    = require('gulp-sass');
-var plumber = require('gulp-plumber');
 var uglify  = require('gulp-uglify');
 var concat  = require('gulp-concat');
 var rename  = require('gulp-rename');
 
-// Variables
-
 //Sources
 var scssFiles = './src/scss/style.scss';
-var js_dev    = [
+var jsFiles    = [
     './src/js/vendor/jquery-3.3.1.min.js',
     './src/js/vendor/bootstrap.min.js',
     './src/js/vendor/slick.min.js',
     './src/js/main.js'
 ];
 
-//CSS destination
-var cssDest      = './src/css';
-var js_dist      = "./dist/js";
-var js_dist_name = "bundle.min.js"
+var srcCopy = {
+    fonts: './src/fonts/**/*.{woff,woff2}',
+    images: './src/img/**/*.{gif,png,jpg}'
+};
+
+//Destination
+var cssDest     = './dist/css';
+var jsDest      = "./dist/js";
+var jsDest_name = "bundle.min.js";
+var distCopy = {
+    fonts: './dist/fonts/',
+    images: './dist/img/'
+};
 
 //Options for development
 var sassDevOptions = {
@@ -28,11 +35,22 @@ var sassDevOptions = {
 
 //Options for production
 var sassProdOptions = {
-    outputStyle: 'compressed'
+    outputStyle: 'compressed',
+    includePaths: [
+        './src/scss/vendor/'
+    ]
 }
 
 // Default task - Run with command 'gulp'
-gulp.task('default', ['sassdev', 'sassprod', 'scripts', 'watch']);
+gulp.task('default', ['sassdev', 'sassprod', 'scripts', 'fonts', 'images', 'watch']);
+
+gulp.task('fonts', function () {
+    return gulp.src(srcCopy.fonts).pipe(gulp.dest(distCopy.fonts));
+  });
+
+  gulp.task('images', function () {
+    return gulp.src(srcCopy.images).pipe(gulp.dest(distCopy.images));
+  });
 
 //Task 'sassdev' - Run with command 'gulp sassdev'
 gulp.task('sassdev', function() {
@@ -51,13 +69,16 @@ gulp.task('sassprod', function() {
 
 // Scripts Task
 gulp.task('scripts', function() {
-    return gulp.src(js_dev)
+    return gulp.src(jsFiles)
         .pipe(uglify())
-        .pipe(concat(js_dist_name))
-        .pipe(gulp.dest(js_dist));
+        .pipe(concat(jsDest_name))
+        .pipe(gulp.dest(jsDest));
 });
 
 //Task 'watch' - Run with command 'gulp watch'
 gulp.task('watch', function(){
     gulp.watch(scssFiles, ['sassdev', 'sassprod']);
+    gulp.watch(jsFiles, ['scripts']);
+    gulp.watch(srcCopy.fonts, ['fonts']);
+    gulp.watch(srcCopy.images, ['images']);
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,37 @@
-$(document).ready(function(){
-    $('.color-carousel').slick({
-        arrows: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        focusOnSelect: true
+(function(){
+    $(document).ready(function(){
+        $('.color-carousel').slick({
+            arrows: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            focusOnSelect: true
+        });
+    
+        $('.product-carousel').slick({
+            arrows: true,
+            slidesToShow: 5
+        });
+    
+        $('.instagram-carousel').slick({
+            arrows: true,
+            slidesToShow: 4
+        });
+    
+        $('.gallery-carousel-main').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            draggable: false,
+            focusOnSelect: false
+          }); 
+          
+          $('.gallery-carousel-nav').slick({
+            slidesToShow: 6,
+            asNavFor: '.gallery-carousel-main',
+            dots: false,
+            focusOnSelect: true,
+            vertical: true
+          });
     });
-
-    $('.product-carousel').slick({
-        arrows: true,
-        slidesToShow: 5
-    });
-
-    $('.instagram-carousel').slick({
-        arrows: true,
-        slidesToShow: 4
-    });
-
-    $('.gallery-carousel-main').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        draggable: false,
-        focusOnSelect: false
-      });
-      
-      $('.gallery-carousel-nav').slick({
-        slidesToShow: 6,
-        asNavFor: '.gallery-carousel-main',
-        dots: false,
-        focusOnSelect: true,
-        vertical: true
-      });
-});
+})();
